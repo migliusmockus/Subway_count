1ae6bd5292890a7ddb1673d0b1f414a195e5f1f5
Miglius Mockus <74502967+migliusmockus@users.noreply.github.com>
2022-02-03 21:11:54 +0100

    Update index.js
    fixed function so that "total" label would change and fixed the count label too so that after every save it changes to 0

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// AFTER UPDATE 
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+let total = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+    total += count
+    document.getElementById("totalSave").innerText = total
+    countEl.innerText = 0
+    count = 0
+}
+
+
+
+/* BEFORE UPDATE
 let countEl = document.getElementById("count-el") //Getting the element "count-el" from Index.html file
 let countTotal = document.getElementById("total-label") // Getting the element "total-label" from Index.html file
 let count = 0 // Setting the count variable to 0
@@ -12,6 +35,7 @@ function save() { // creating a function which will change the count variable to
     count = 0
     countEl.innerText = count
 }
+*/
 
 
 
